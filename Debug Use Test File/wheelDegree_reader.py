74759c56bf093ba22632a299a52a5652d0a384ee
Gmemster <chongyaoteh1@gmail.com>
2024-07-06 08:41:52 +0800

    updated some code

--- Debug Use Test File/wheelDegree_reader.py
+++ Debug Use Test File/wheelDegree_reader.py
@@ -1,10 +1,13 @@
 from Robosport_FuncMain import*
 
+totalDegrees = 0
+totalDegrees=(abs(MotorC.angle())+abs(MotorD.angle()))/2
+
 MotorC.reset_angle(0)
 MotorD.reset_angle(0)
 
 while True:
-    print(MotorC.angle,MotorD.angle)
+    print(totalDegrees)
 
     if hub.buttons.pressed() = (Button.LEFT or Button.RIGHT):
         MotorC.reset_angle(0)
