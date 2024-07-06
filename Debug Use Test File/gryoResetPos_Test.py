1a5ac7c4a7708d5e9705d0cb6390796b319c4328
Gmemster <chongyaoteh1@gmail.com>
2024-07-06 16:46:33 +0800

    added new code

--- Debug Use Test File/gryoResetPos_Test.py
+++ Debug Use Test File/gryoResetPos_Test.py
@@ -1,4 +1,9 @@
-from Robosport_FuncMain import *
+from Robosport_FuncMain import*
 
+
+# Print gryo Heading
+if hub.buttons.pressed() = (Button.BLUETOOTH):
+    print(hub.imu.heading())
+    
 while True:
     GryoMove(0,0)
--- Debug Use Test File/wheelDegree_reader.py
+++ Debug Use Test File/wheelDegree_reader.py
@@ -6,9 +6,13 @@ totalDegrees=(abs(MotorC.angle())+abs(MotorD.angle()))/2
 MotorC.reset_angle(0)
 MotorD.reset_angle(0)
 
-while True:
-    print(totalDegrees)
+if hub.buttons.pressed() = (Button.LEFT):
+        print(totalDegrees)
 
-    if hub.buttons.pressed() = (Button.LEFT or Button.RIGHT):
+if hub.buttons.pressed() = (Button.RIGHT):
         MotorC.reset_angle(0)
         MotorD.reset_angle(0)
+
+
+    
+    
