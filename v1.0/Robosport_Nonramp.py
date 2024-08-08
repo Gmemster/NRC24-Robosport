d97e36e4a49071f983658c397d97f13b35f9615b
Gmemster <chongyaoteh1@gmail.com>
2024-08-08 18:11:09 +0800

    Updated Gryo to Gyro

--- v1.0/Robosport_Nonramp.py
+++ v1.0/Robosport_Nonramp.py
@@ -1,31 +1,31 @@
 from Robosport_FuncMain import*
 
-'gryo respawn test'
+'Gyro respawn test'
 # while True:
-#     GryoMove(0,0)
+#     GyroMove(0,0)
 
 while True:
-    GryoReset()
-    GryoMoveTime(0,100,1.5)
+    GyroReset()
+    GyroMoveTime(0,100,1.5)
     MotorE.dc(-100)
     wait(1200)
     MotorE.stop()
-    GryoMoveTime(0,-30,0.2)
-    GryoMoveTime(85,0,0.1)
-    GryoMoveTime(85,100,0.7)#turn to the right wall
-    GryoReset()
-    GryoMoveTime(0,-30,0.2)
-    GryoMoveTime(85,0,0.1)
-    GryoMoveTime(85,100,1.8)#straights to the middle wall
-    GryoReset()
-    GryoMoveTime(0,-30,-0.2)
-    GryoMoveTime(85,0,0.1)
-    GryoMoveTime(85,100,1)#facing left wall
-    GryoReset()
-    GryoMoveTime(0,-30,0.2)
-    GryoMoveTime(85,0,0.1)
-    GryoMoveTime(85,100,1.5)
-    GryoReset()
-    GryoMoveTime(0,-30,0.2)
-    GryoMoveTime(-30,-50,1.2)#middle field
-    GryoMoveTime(0,-50,2)
+    GyroMoveTime(0,-30,0.2)
+    GyroMoveTime(85,0,0.1)
+    GyroMoveTime(85,100,0.7)#turn to the right wall
+    GyroReset()
+    GyroMoveTime(0,-30,0.2)
+    GyroMoveTime(85,0,0.1)
+    GyroMoveTime(85,100,1.8)#straights to the middle wall
+    GyroReset()
+    GyroMoveTime(0,-30,-0.2)
+    GyroMoveTime(85,0,0.1)
+    GyroMoveTime(85,100,1)#facing left wall
+    GyroReset()
+    GyroMoveTime(0,-30,0.2)
+    GyroMoveTime(85,0,0.1)
+    GyroMoveTime(85,100,1.5)
+    GyroReset()
+    GyroMoveTime(0,-30,0.2)
+    GyroMoveTime(-30,-50,1.2)#middle field
+    GyroMoveTime(0,-50,2)
